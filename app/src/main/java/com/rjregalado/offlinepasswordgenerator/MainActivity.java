5031d7b8dea24eaa1007e78d6d108e9438cac5be
leetkrew <rjfiles2000@yahoo.com>
2018-02-13 16:07:39 +0800

    ads

--- app/src/main/java/com/rjregalado/offlinepasswordgenerator/MainActivity.java
+++ app/src/main/java/com/rjregalado/offlinepasswordgenerator/MainActivity.java
@@ -13,9 +13,30 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    private void CopyToClipBoard(EditText txtGeneratedPassword) {
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("generatedPassword", txtGeneratedPassword.getText());
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(getApplicationContext(), "Copied to Clipboard", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private View.OnClickListener btnCopy_Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            CopyToClipBoard ((EditText)findViewById(R.id.txtGeneratedPassword));
+        }
+    };
 
 
     private View.OnClickListener btnGenerateAndCopy_Click = new View.OnClickListener() {
@@ -40,17 +61,9 @@ public class MainActivity extends AppCompatActivity {
                     , boolProgrammingCharacters.isChecked()
                     , boolSimilarCharacters.isChecked()
                     , boolUpperCase.isChecked()
-
-
             )) ;
 
-
-            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-            ClipData clip = ClipData.newPlainText("generatedPassword", txtGeneratedPassword.getText());
-            clipboard.setPrimaryClip(clip);
-
-            Toast.makeText(getApplicationContext(), "Password Generated", Toast.LENGTH_LONG).show();
-
+            CopyToClipBoard ((EditText)findViewById(R.id.txtGeneratedPassword));
         }
     };
 
@@ -78,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
 
             )) ;
-            Toast.makeText(getApplicationContext(), "Password Generated", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Password Generated", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -89,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btngenerate = (Button)findViewById(R.id.btngenerate);
         Button btnGenerateAndCopy = (Button)findViewById(R.id.btnGenerateAndCopy);
+        Button btnCopy = (Button)findViewById(R.id.btnCopy);
         final EditText txtLength = (EditText)findViewById(R.id.txtLength);
 
 
@@ -106,5 +120,15 @@ public class MainActivity extends AppCompatActivity {
         txtLength.setText("12");
         btngenerate.setOnClickListener(btnGenerate_Click);
         btnGenerateAndCopy.setOnClickListener(btnGenerateAndCopy_Click);
+        btnCopy.setOnClickListener(btnCopy_Click);
+
+        MobileAds.initialize(this, "ca-app-pub-0525976082076681~1925831515");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+
     }
 }
